b52495c65e5d8a8a0e923aac8309d1455d565436
Mario Matos <mariomatos889@gmail.com>
2023-06-11 19:31:13 -0400

    added hero style image and content

---
+++ client/yadiweddingregistration/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Yadeiri & Ordanell</h1>
+          <h2>Listado de Boda</h2>
+          <p>Para nosotros es un placer que seas parte de este momento tan importante en nuestras vidas.</p>
+          <p>Informaction de cuenta: </p>
+          <p>Banco Popular - Cuenta de ahorros<br/>828481366<br/>A nombre de Yadeiri L. Hernández/ Ordanell J. Mejía</p>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<p>*/}
+        {/*  Edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
+      </header>
+    </div>
+  );
+}
+
+export default App;
